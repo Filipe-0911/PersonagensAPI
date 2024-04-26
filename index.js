68329b8918f641190e292b91cbdd1f4f544e1d1f
Filipe <fbianchi.andrade@gmail.com>
2024-04-26 12:10:37 -0300

    adicionada mais uma camada de segurança com a lib helmet

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
+
 const rotaJogo = require('./rotas/jogo-rotas');
 const rotaAuth = require('./rotas/auth-rotas');
 
 const app = express();
+
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors({origin:'*'}));
 app.use('/personagens', rotaJogo);
